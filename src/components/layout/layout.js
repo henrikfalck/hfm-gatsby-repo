a52a9ad95c1e44559392cd465c7d22680d6fbc9c
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-04-21 15:23:38 +0200

    Added layout component

---
+++ src/components/layout/layout.js
@@ -0,0 +1,18 @@
+import { React } from "react"
+import PropTypes from "prop-types"
+import "../../style/styles.scss"
+import "./layout.scss"
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <main id="layout">{children}</main>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
