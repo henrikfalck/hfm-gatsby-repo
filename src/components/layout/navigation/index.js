a0f180ef247f16dd8cf6fdd83cc91ede9716c9b0
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-06-09 12:46:17 +0200

    Added basic navigation component

---
+++ src/components/layout/navigation/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Navigation = () => (
+  <ul className="navigation">
+    <Link to="/">Forside</Link>
+    <Link to="/sandbox">Sandbox</Link>
+  </ul>
+)
+
+export default Navigation
