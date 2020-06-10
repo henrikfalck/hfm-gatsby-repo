ede2cd7441460cb0b868266be0ca87a62bf6f10c
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-06-10 15:20:35 +0200

    Added basic header

---
+++ src/components/layout/header/header.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+import Wrapper from "../wrapper"
+import Navigation from "../navigation"
+const Header = () => (
+  <header>
+    <Wrapper>
+      <Navigation />
+    </Wrapper>
+  </header>
+)
+
+export default Header
