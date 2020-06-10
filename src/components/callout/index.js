5a8c4e4c850eadd0fa882a48bdf59bdecf7136a6
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-06-10 15:20:04 +0200

    Added new component, needs more work

---
+++ src/components/callout/index.js
@@ -0,0 +1,34 @@
+import React from "react"
+import Icon from "../icons"
+import "./callout.scss"
+function Callout({ title, type, children, center }) {
+  const hasIcon = type
+
+  if (hasIcon) {
+    return (
+      <div
+        className={`callout ${"callout-type__" + type || ""}`}
+        style={{ margin: center ? "0 auto" : "0" }}
+      >
+        <div className="callout__icon">
+          <Icon type={type} />
+        </div>
+        <div className="callout__content">
+          <h2>{title}</h2>
+          {children}
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="callout" style={{ margin: center ? "0 auto" : "0" }}>
+      <div className="callout__content">
+        <h2>{title}</h2>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Callout
