d3f9355a2bcc2b988c0471268cd4c31afb0be02d
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-07-29 14:08:48 +0200

    Imported SCSS file and exported component

--- src/components/layout/footer/footer.js
+++ src/components/layout/footer/footer.js
@@ -1,5 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 import { Flexbox, FlexChild } from "../flexbox/index"
+import "./footer.scss"
 
 const Footer = () => <footer>Footer</footer>
+
+export default Footer
