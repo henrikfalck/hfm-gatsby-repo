f4bdb479a9f87f2c0d4b0e717c396a62fa136098
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-07-29 14:05:32 +0200

    Added props for rendering icons

--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -1,8 +1,15 @@
 import React from "react"
+import Icon from "../icons/index"
 import "./buttons.scss"
 
-const Button = ({ className, children, iconRight, iconLeft }) => (
-  <button className={`button ${className || ""}`}>{children}</button>
+const Button = ({ children, hasIcon, iconRight, iconLeft, type }) => (
+  <button className={`button ${(hasIcon && "has-icon") || ""}`}>
+    {iconLeft && <Icon type={type} variant={iconLeft} className="icon-left" />}
+    {children}
+    {iconRight && (
+      <Icon type={type} variant={iconRight} className="icon-right" />
+    )}
+  </button>
 )
 
 export default Button
