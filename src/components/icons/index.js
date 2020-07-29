74d89fb432dc97fc9634da948646d35e4df6e488
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-07-29 14:08:02 +0200

    Fixed icons

---
+++ src/components/icons/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react"
+
+class Icon extends Component {
+  // Identify which icon to render
+  renderIcon = path => {
+    switch (path) {
+      case "information":
+        return "M12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm1,16H11V16h2Zm0-4H11V7h2Z"
+        break
+      case "warning":
+        return "M12,3,2,21H22Zm1,16H11V17h2Zm-2-4V8h2v7Z"
+        break
+      case "arrow-down":
+        return "M19.071 10.561L13 16.632V4h-2v12.632l-6.071-6.071-1.414 1.414 7.071 7.071L12 20.46l1.414-1.414 7.071-7.071-1.414-1.414z"
+        break
+      case "arrow-up":
+        return "M19.071 10.561L13 16.632V4h-2v12.632l-6.071-6.071-1.414 1.414 7.071 7.071L12 20.46l1.414-1.414 7.071-7.071-1.414-1.414z"
+        break
+      // no default
+    }
+  }
+
+  render() {
+    return (
+      <div className={`icon ${this.props.className}`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={`${this.props.size || "20"}`}
+          height={`${this.props.size || "20"}`}
+          viewBox="0 0 24 24"
+        >
+          <path d={this.renderIcon(this.props.variant)} />
+        </svg>
+      </div>
+    )
+  }
+}
+
+export default Icon
