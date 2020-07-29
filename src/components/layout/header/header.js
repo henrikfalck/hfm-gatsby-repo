60ce75f04e80a4c5020a5b7234128db82e678943
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-07-29 14:09:00 +0200

    Imported SCSS file

--- src/components/layout/header/header.js
+++ src/components/layout/header/header.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import Wrapper from "../wrapper"
 import Navigation from "../navigation"
+import "./header.scss"
 const Header = () => (
   <header>
     <Wrapper>
