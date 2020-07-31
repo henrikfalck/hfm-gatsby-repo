ebb4831bc1b0747f0fc255103b9784128b891745
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-07-31 03:48:27 +0200

    Renamed file to index

--- src/components/layout/footer/footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { Link } from "gatsby"
-import { Flexbox, FlexChild } from "../flexbox/index"
-import "./footer.scss"
-
-const Footer = () => <footer>Footer</footer>
-
-export default Footer
