bb465b66e9fe03cc72725642c77280e1cbd9a4d2
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-08-05 14:05:15 +0200

    Add Header and Footer to comp

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,12 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Header from "../layout/header"
+import Footer from "../layout/footer"
 import "../../style/styles.scss"
 import "./layout.scss"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Header />
       <main id="layout">{children}</main>
+      <Footer />
     </>
   )
 }
