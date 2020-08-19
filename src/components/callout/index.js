0120b718125feb9e43e3c4a3c99278f649683ac7
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-08-19 17:40:11 +0200

    Simplifie `Callout`

--- src/components/callout/index.js
+++ src/components/callout/index.js
@@ -2,35 +2,23 @@ import React from "react"
 import PropTypes from "prop-types"
 import Icon from "../icon"
 import "./callout.scss"
-function Callout({ title, type, children, center }) {
-  const hasIcon = type
 
-  if (hasIcon) {
-    return (
-      <div
-        className={`callout ${"callout-type__" + type || ""}`}
-        style={{ margin: center ? "0 auto" : "0" }}
-      >
-        <div className="callout__icon">
-          <Icon variant={type} />
-        </div>
-        <div className="callout__content">
-          <h2>{title}</h2>
-          {children}
-        </div>
+const Callout = ({ center, children, title, type }) => (
+  <div
+    className={`callout ${"callout-type__" + type || ""}`}
+    style={{ margin: center ? "0 auto" : "0" }}
+  >
+    {hasIcon ? (
+      <div className="callout__icon">
+        <Icon variant={type} />
       </div>
-    )
-  }
-
-  return (
-    <div className="callout" style={{ margin: center ? "0 auto" : "0" }}>
-      <div className="callout__content">
-        <h2>{title}</h2>
-        {children}
-      </div>
+    ) : null}
+    <div className="callout__content">
+      <h2>{title}</h2>
+      {children}
     </div>
-  )
-}
+  </div>
+)
 
 export default Callout
 
